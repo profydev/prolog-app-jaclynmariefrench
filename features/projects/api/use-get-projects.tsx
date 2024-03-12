e9fbf5def3f1accd5c863c2e462b433b1a6899b1
Jaclyn French <77642588+jaclynmariefrench@users.noreply.github.com>
2024-03-12 15:26:54 -0700

    Project list incorrect status (#8)
    * added switch to transform api data
    
    * added cypress test for badges

--- features/projects/api/use-get-projects.tsx
+++ features/projects/api/use-get-projects.tsx
@@ -1,7 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProjects } from "@api/projects";
-import type { Project } from "@api/projects.types";
+import { ProjectStatus, type Project } from "@api/projects.types";
+
+// Converts project status data from 'info' to 'stable' and 'error' to 'critical'
+function transformStatus(status: string): ProjectStatus {
+  switch (status) {
+    case "info":
+      return ProjectStatus.stable;
+    case "error":
+      return ProjectStatus.critical;
+    default:
+      return status as ProjectStatus;
+  }
+}
 
 export function useGetProjects() {
-  return useQuery<Project[], Error>(["projects"], getProjects);
+  return useQuery<Project[], Error>(["projects"], getProjects, {
+    select: (data) =>
+      data.map((project) => ({
+        ...project,
+        status: transformStatus(project.status),
+      })),
+  });
 }
