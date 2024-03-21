1a38267aa4273caaabeb1979298163ac1d2864ff
Jackie French <jaclynmariefrench@gmail.com>
2024-03-21 17:01:42 -0500

    finished styling error banner

---
+++ features/projects/components/project-error/project-error.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Meta, StoryFn } from "@storybook/react";
+import { ProjectError } from "./project-error";
+
+export default {
+  title: "project/ProjectError",
+  component: ProjectError,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+} as Meta<typeof ProjectError>;
+
+const Template: StoryFn<typeof ProjectError> = () => (
+  <div style={{ width: 1440 }}>
+    <ProjectError />
+  </div>
+);
+export const Default = Template.bind({});
---
+++ features/projects/components/project-error/project-error.tsx
@@ -0,0 +1,31 @@
+import styles from "./project-error.module.scss";
+import { Button } from "@features/ui";
+
+export function ProjectError() {
+  return (
+    <div className={styles["error-container"]}>
+      <div className={styles["left-banner"]}>
+        <div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={"/icons/alert-circle.svg"}
+            alt="alert icon"
+            className={styles["alert-circle"]}
+          />
+        </div>
+        <p>There was a problem while loading the project data</p>
+      </div>
+
+      {/*will need to be a button*/}
+      <Button className={styles["right-banner"]}>
+        <p>Try again</p>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={"/icons/arrow-right.svg"}
+          alt="arrow"
+          className={styles["error-arrow"]}
+        />
+      </Button>
+    </div>
+  );
+}
