96b4e6d05b75525a37c08194cfad13735d78ff9d
Jackie French <jaclynmariefrench@gmail.com>
2024-03-29 10:50:35 -0500

    button redo size

--- features/ui/button/button.stories.tsx
+++ features/ui/button/button.stories.tsx
@@ -1,40 +1,73 @@
-import React from "react";
-import { Meta, StoryFn } from "@storybook/react";
-import { Button } from "./button";
+import { Meta, StoryObj } from "@storybook/react";
+import { Button, ButtonSize } from "./button";
 
-export default {
-  title: "UI/Button",
-  component: Button,
-  argTypes: {
-    size: {
-      options: ["small", "medium", "large", "xlarge"],
-      control: { type: "radio" },
-    },
-    color: {
-      options: [
-        "primary",
-        "secondary",
-        "gray",
-        "empty",
-        "empty-gray",
-        "error",
-        "empty-error",
-      ],
-      control: { type: "radio" },
-    },
-    // icon: {
-    //     options: ['none', 'leading', 'trailing', 'only'],
-    //     control: { type: 'select' }
-    // }
-  },
-} as Meta<typeof Button>;
+const meta: Meta<typeof Button> = { title: "UI/Button", component: Button };
 
-const Template: StoryFn = (args) => <Button {...args} />;
+export default meta;
 
-export const Default = Template.bind({});
-Default.args = {
-  children: "Button",
-  size: "small",
-  color: "primary",
-  // icon: "none"
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: { children: "Button CTA" },
 };
+
+export const Small: Story = {
+  args: { ...Default.args, size: ButtonSize.Small },
+};
+
+export const Medium: Story = {
+  args: { ...Default.args, size: ButtonSize.Medium },
+};
+
+export const Large: Story = {
+  args: { ...Default.args, size: ButtonSize.Large },
+};
+
+export const XLarge: Story = {
+  args: { ...Default.args, size: ButtonSize.XLarge },
+};
+
+// import React from "react";
+// import { Meta, StoryFn } from "@storybook/react";
+// import { Button } from "./button";
+// import { ReactComponent as IconPlaceholder} from '/Users/jaclynfrench/workspace/react-course/prolog-app-jaclynmariefrench/public/icons/message.svg';
+
+// export default {
+//   title: "UI/Button",
+//   component: Button,
+//   argTypes: {
+//     size: {
+//       options: ["small", "medium", "large", "xlarge"],
+//       control: { type: "radio" },
+//     },
+//     color: {
+//       options: [
+//         "primary",
+//         "secondary",
+//         "gray",
+//         "empty",
+//         "empty-gray",
+//         "error",
+//         "empty-error",
+//       ],
+//       control: { type: "radio" },
+//     },
+//     iconPosition: {
+//         options: ['none', 'leading', 'trailing', 'only'],
+//         control: { type: 'select' }
+//     }
+//   },
+// } as Meta<typeof Button>;
+
+// const Template: StoryFn = (args) => <Button {...args} />;
+
+// export const Default = Template.bind({});
+// Default.args = {
+//   children: "Button",
+//   size: "small",
+//   color: "primary",
+//   // icon: <IconPlaceholder/>,
+//   iconPosition: "none"
+// };
+
+// // console.log(IconPlaceholder)
--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,31 +1,64 @@
-import { ButtonHTMLAttributes } from "react";
 import classNames from "classnames";
+import { UnstyledButton } from "./unstyled-button";
 import styles from "./button.module.scss";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: "small" | "medium" | "large" | "xlarge";
-  color?:
-    | "primary"
-    | "secondary"
-    | "gray"
-    | "empty"
-    | "empty-gray"
-    | "error"
-    | "empty-error";
-  // icon?: "none" | "leading" | "trailing" | "only";
+export enum ButtonSize {
+  Small = "small",
+  Medium = "medium",
+  Large = "large",
+  XLarge = "xlarge",
 }
 
-export function Button({ size, color, ...props }: ButtonProps) {
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  size?: ButtonSize;
+  children: React.ReactNode;
+};
+
+export function Button({
+  className,
+  size = ButtonSize.Medium,
+  ...props
+}: ButtonProps) {
   return (
-    <button
+    <UnstyledButton
+      className={classNames(styles.button, styles[size], className)}
       {...props}
-      className={classNames(
-        styles.button,
-        size && styles[size],
-        color && styles[color],
-        // icon !== 'none' ? styles[icon] : undefined,
-        props.className,
-      )}
     />
   );
 }
+
+// import { ButtonHTMLAttributes, ReactNode } from "react";
+// import classNames from "classnames";
+// import styles from "./button.module.scss";
+
+// interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+//   size?: "small" | "medium" | "large" | "xlarge";
+//   color?:
+//     | "primary"
+//     | "secondary"
+//     | "gray"
+//     | "empty"
+//     | "empty-gray"
+//     | "error"
+//     | "empty-error";
+//   icon?: ReactNode;
+//   iconPosition?: "none" | "leading" | "trailing" | "only";
+// }
+
+// export function Button({ size, color, icon, iconPosition, children, ...props }: ButtonProps) {
+//   return (
+//     <button
+//       {...props}
+//       className={classNames(
+//         styles.button,
+//         size && styles[size],
+//         color && styles[color],
+//         props.className,
+//       )}
+//     >
+//       {(iconPosition === "leading" || iconPosition === "only") && icon}
+//       {iconPosition !== "only" && children}
+//       {iconPosition === "trailing" && icon}
+//       </button>
+//   );
+// }
