fb23c4eb0f1e2e4da448c5e1a81d1c9aca21306c
Jackie French <jaclynmariefrench@gmail.com>
2024-04-08 15:26:31 -0500

    reuploaded dependencies and finished adding checked state

--- features/ui/checkbox/checkbox.stories.tsx
+++ features/ui/checkbox/checkbox.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { useArgs } from "@storybook/preview-api";
 import { Checkbox } from "./checkbox";
 
 const meta: Meta<typeof Checkbox> = {
@@ -11,5 +12,15 @@ export default meta;
 type Story = StoryObj<typeof Checkbox>;
 
 export const Default: Story = {
-  args: { children: "Label" },
+  args: { children: "Label", checked: false },
+  render: function Render(args) {
+    const [{ checked }, updateArgs] = useArgs();
+    return (
+      <Checkbox
+        {...args}
+        checked={checked}
+        onChange={(e) => updateArgs({ checked: e.target.checked })}
+      />
+    );
+  },
 };
--- features/ui/checkbox/checkbox.tsx
+++ features/ui/checkbox/checkbox.tsx
@@ -7,14 +7,30 @@ export function Checkbox({
   children,
   className,
   style,
+  checked,
   ...otherProps
 }: CheckboxProps) {
   return (
     <label className={classNames(styles.container, className)} style={style}>
-      <input {...otherProps} type="checkbox" />{" "}
-      {/* <- this is the hidden checkbox*/}
-      <span className={classNames(styles.checkbox)} />{" "}
-      {/* <- this is the custom shown checkbox*/}
+      <input {...otherProps} type="checkbox" checked={checked} />
+      {/* ^^ this is the hidden checkbox*/}
+      <span className={classNames(styles.checkbox)}>
+        {/* ^^ this is the custom shown checkbox*/}
+        <svg
+          className={styles.check}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 12 12"
+          fill="none"
+        >
+          <path
+            d="M10 3L4.5 8.5L2 6"
+            stroke="currentColor"
+            strokeWidth="1.6666"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </span>
       {children}
     </label>
   );
