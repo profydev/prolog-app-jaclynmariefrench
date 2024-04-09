36e95de07c7a500312e74fdea2a510f45c791772
Jackie French <jaclynmariefrench@gmail.com>
2024-04-09 15:37:17 -0500

    ref support

--- features/ui/checkbox/checkbox.tsx
+++ features/ui/checkbox/checkbox.tsx
@@ -1,68 +1,76 @@
 import classNames from "classnames";
 import styles from "./checkbox.module.scss";
-import { useEffect, useRef } from "react";
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
 
 type CheckboxProps = React.InputHTMLAttributes<HTMLInputElement> & {
   indeterminate?: boolean;
 };
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  function Checkbox(
+    { children, className, style, checked, indeterminate, ...otherProps },
+    ref,
+  ) {
+    const inputRef = useRef<HTMLInputElement>(null);
 
-export function Checkbox({
-  children,
-  className,
-  style,
-  checked,
-  indeterminate,
-  ...otherProps
-}: CheckboxProps) {
-  const inputRef = useRef<HTMLInputElement>(null);
+    useImperativeHandle<HTMLInputElement | null, HTMLInputElement | null>(
+      ref,
+      () => inputRef.current,
+      [],
+    );
 
-  useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.indeterminate = indeterminate ?? false;
-    }
-  }, [indeterminate]);
+    useEffect(() => {
+      if (inputRef.current) {
+        inputRef.current.indeterminate = indeterminate ?? false;
+      }
+    }, [indeterminate]);
 
-  return (
-    <label className={classNames(styles.container, className)} style={style}>
-      <input {...otherProps} type="checkbox" checked={checked} ref={inputRef} />
-      {/* ^^ input is the hidden checkbox*/}
-      <span
-        className={classNames(
-          styles.checkbox,
-          indeterminate && styles.indeterminate,
-        )}
-      >
-        {/* span is is the custom shown checkbox*/}
-        <svg
-          className={styles.indeterminateCheck}
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 12 12"
-          fill="none"
+    return (
+      <label className={classNames(styles.container, className)} style={style}>
+        <input
+          {...otherProps}
+          type="checkbox"
+          checked={checked}
+          ref={inputRef}
+        />
+        {/* ^^ input is the hidden checkbox*/}
+        <span
+          className={classNames(
+            styles.checkbox,
+            indeterminate && styles.indeterminate,
+          )}
         >
-          <path
-            d="M2.5 6H9.5"
-            stroke="currentColor"
-            strokeWidth="1.66666"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-        <svg
-          className={styles.check}
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 12 12"
-          fill="none"
-        >
-          <path
-            d="M10 3L4.5 8.5L2 6"
-            stroke="currentColor"
-            strokeWidth="1.6666"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </span>
-      {children}
-    </label>
-  );
-}
+          {/* span is is the custom shown checkbox*/}
+          <svg
+            className={styles.indeterminateCheck}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 12 12"
+            fill="none"
+          >
+            <path
+              d="M2.5 6H9.5"
+              stroke="currentColor"
+              strokeWidth="1.66666"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <svg
+            className={styles.check}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 12 12"
+            fill="none"
+          >
+            <path
+              d="M10 3L4.5 8.5L2 6"
+              stroke="currentColor"
+              strokeWidth="1.6666"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </span>
+        {children}
+      </label>
+    );
+  },
+);
