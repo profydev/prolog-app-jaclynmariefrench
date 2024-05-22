6a6170ba1d5b5582e0024659edd0785ff82b3e91
Jackie French <jaclynmariefrench@gmail.com>
2024-05-22 14:27:26 -0500

    added styling for issue no results

--- features/issues/components/issue-filter/issue-no-results.tsx
+++ features/issues/components/issue-filter/issue-no-results.tsx
@@ -1,18 +1,25 @@
-import { Button } from "@features/ui";
+import { Button, ButtonSize } from "@features/ui";
 import styles from "./issue-no-results.module.scss";
 
 export function IssueNoResults() {
   return (
     <div className={styles.noResultContainer}>
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src="/icons/illustration.svg" alt="cloud and search glass" />
-      <h2>No issues found</h2>
-      <p>
-        Either the filters you selected are too restrictive or there are no
-        issues for your projects.
-      </p>
-      <div>
-        <Button />
+      <div>Filter here</div>
+      <div className={styles.contentContainer}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/icons/illustration.svg" alt="cloud and search glass" />
+        <div className={styles.textContainer}>
+          <h2 className={styles.header}>No issues found</h2>
+          <p className={styles.paragraph}>
+            Either the filters you selected are too restrictive or there are no
+            issues for your projects.
+          </p>
+        </div>
+        <div>
+          <Button size={ButtonSize.Large} className={styles.button}>
+            Clear Filters
+          </Button>
+        </div>
       </div>
     </div>
   );
