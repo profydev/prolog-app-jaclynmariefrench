36b400884f8edc8f7cc14ff75d4d961a5c14e4ab
Jackie French <jaclynmariefrench@gmail.com>
2024-05-30 14:23:03 -0500

    setting up issue-filter cypress test

--- cypress/e2e/issue-list.cy.ts
+++ cypress/e2e/issue-list.cy.ts
@@ -2,21 +2,32 @@ import mockIssues1 from "../fixtures/issues-page-1.json";
 import mockIssues2 from "../fixtures/issues-page-2.json";
 import mockIssues3 from "../fixtures/issues-page-3.json";
 
+// Allows filtering for “Unresolved” and “Resolved” issues
+// Allows filtering for level “Error”, “Warning”, and “Info”
+// Allows filtering for any existing project by (partial) project name
+// Every dropdown can be empty (no value selected) to remove a filter
+// Issues are displayed according to the filters
+// Each filter should persist after the page is refreshed
+// The user should be able to share the page including the currently selected filters
+
 describe("Issue List", () => {
   beforeEach(() => {
     // setup request mocks
     cy.intercept("GET", "https://prolog-api.profy.dev/project", {
       fixture: "projects.json",
     }).as("getProjects");
-    cy.intercept("GET", "https://prolog-api.profy.dev/issue?page=1", {
+    cy.intercept("GET", "https://prolog-api.profy.dev/issue?page=1*", {
       fixture: "issues-page-1.json",
     }).as("getIssuesPage1");
-    cy.intercept("GET", "https://prolog-api.profy.dev/issue?page=2", {
+    cy.intercept("GET", "https://prolog-api.profy.dev/issue?page=2*", {
       fixture: "issues-page-2.json",
     }).as("getIssuesPage2");
-    cy.intercept("GET", "https://prolog-api.profy.dev/issue?page=3", {
+    cy.intercept("GET", "https://prolog-api.profy.dev/issue?page=3*", {
       fixture: "issues-page-3.json",
     }).as("getIssuesPage3");
+
+    // wait for intercepts to be set up
+    cy.wait(0);
 
     // open issues page
     cy.visit(`http://localhost:3000/dashboard/issues`);
@@ -57,18 +68,21 @@ describe("Issue List", () => {
 
       // test navigation to second page
       cy.get("@next-button").click();
+      cy.wait("@getIssuesPage2");
       cy.get("@prev-button").should("not.have.attr", "disabled");
       cy.contains("Page 2 of 3");
       cy.get("tbody tr:first").contains(mockIssues2.items[0].message);
 
       // test navigation to third and last page
       cy.get("@next-button").click();
+      cy.wait("@getIssuesPage3");
       cy.get("@next-button").should("have.attr", "disabled");
       cy.contains("Page 3 of 3");
       cy.get("tbody tr:first").contains(mockIssues3.items[0].message);
 
       // test navigation back to second page
       cy.get("@prev-button").click();
+      cy.wait("@getIssuesPage2");
       cy.get("@next-button").should("not.have.attr", "disabled");
       cy.contains("Page 2 of 3");
       cy.get("tbody tr:first").contains(mockIssues2.items[0].message);
